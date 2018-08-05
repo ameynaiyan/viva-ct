d7e04f302dc9b104b39d4b301196964f89920bbb
Amey Naiyan <ameyd9@gmail.com>
2018-08-05 13:14:26 -0400

    Updated constructor and event handler

--- src/viva-ct-src.js
+++ src/viva-ct-src.js
@@ -9,5 +9,33 @@
  *
  */
 
+ class VivaCT {
+ 	constructor(q) {
+ 		let nodeList = document.querySelector(q);
+ 		this.el = nodeList.length==undefined?[nodeList]:[...nodeList];
+ 	}
 
- 
+ 	static get animationStart() {
+ 		return 'webkitAnimationStart mozAnimationStart MSAnimationStart oanimationstart animationstart'
+ 	}
+
+ 	static get animationEnd() {
+ 		return 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
+ 	}
+
+ 	static get animationIteration() {
+ 		return 'webkitAnimationIteration mozAnimationIteration MSAnimationIteration oanimationiteration animationiteration'
+ 	}
+
+ 	onStart(callback) {
+ 		this.el.addEventListener(VivaCT.animationStart, callback);
+ 	}
+
+ 	onEnd(callback) {
+ 		this.el.addEventListener(VivaCT.animationEnd, callback);
+ 	}
+
+ 	onStep(callback) {
+ 		this.el.addEventListener(VivaCT.animationIteration, callback);
+ 	}
+ }
