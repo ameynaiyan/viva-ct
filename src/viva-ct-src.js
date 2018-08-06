bdc5e111269589f5246f63dbbc2908c800186e9a
Amey Naiyan <ameyd9@gmail.com>
2018-08-06 19:22:25 -0400

    Updated class

--- src/viva-ct-src.js
+++ src/viva-ct-src.js
@@ -14,6 +14,8 @@
  	constructor(q) {
  		let nodeList = document.querySelector(q);
  		this.el = nodeList.length==undefined?[nodeList]:[...nodeList];
+ 		this.name = '';
+ 		this.currentIteration = 0;
  	}
 
  	static get vendorPrefixes() {
@@ -106,31 +108,76 @@
  		return 'normal'
  	}
 
+ 	static get defaultPlayState() {
+ 		return 'running'
+ 	}
+
  	static get defaultConfig() {
  		return {
  			q: null,
- 			duration: '0.75s',
- 			timingFunction: 'linear',
- 			delay: '0s',
- 			iterationCount: 1,
- 			direction: 'normal'
+ 			duration: VivaCT.defaultDuration,
+ 			timingFunction: VivaCT.defaultTimingFunction,
+ 			delay: VivaCT.defaultDelay,
+ 			iterationCount: VivaCT.iterationCount,
+ 			direction: VivaCT.direction,
+ 			keyframes: [],
+ 			onStart: VivaCT._emptyFn,
+ 			onEnd: VivaCT._emptyFn,
+ 			onStep: VivaCT._emptyFn
  		}
  	}
 
- 	onStart(callback) {
- 		this.el.map(el => el.addEventListener(VivaCT.animationStart, callback));
+
+ 	static _generateRandomName() {
+ 		return 'anim-'+ Math.floor(Math.random() * 1000000000);
  	}
 
- 	onEnd(callback) {
- 		this.el.map(el => el.addEventListener(VivaCT.animationEnd, callback));
+ 	static _emptyFn() {
+ 		return;
  	}
 
- 	onStep(callback) {
- 		this.el.map(el => el.addEventListener(VivaCT.animationIteration, callback));
+ 	static _initAnimation(config){
+ 		this.name = VivaCT._generateRandomName();
+ 		this._createKeyframes(config.keyframes);
+ 		this._createClass(config.duration, config.timingFunction, config.delay, config.iterationCount, config.direction, config.playState)
+ 		this._onStart(config.onStart);
+ 		this._onStep(config.onStep);
+ 		this._onEnd(config.onEnd);
+ 	}
+
+ 	_createKeyframes(keyframes) {
+ 		
+ 	}
+
+ 	_createClass(duration = VivaCT.defaultDuration, timing = VivaCT.defaultTimingFunction, delay = VivaCT.defaultDelay, iterationCount = VivaCT.defaultIterationCount, direction = VivaCT.defaultDirection, playState = VivaCT.defaultPlayState) {
+ 		
+ 	}
+
+ 	_clearDom() {
+
+ 	}
+
+ 	_onStart(callback = VivaCT._emptyFn) {
+ 		this.el.map(el => el.addEventListener(VivaCT.animationStart, () => {
+ 			callback.call(this);
+ 		}).bind(this));
+ 	}
+
+ 	_onEnd(callback = VivaCT._emptyFn) {
+ 		this.el.map(el => el.addEventListener(VivaCT.animationEnd, () => {
+ 			callback.call(this);
+ 		}).bind(this));
+ 	}
+
+ 	_onStep(callback = VivaCT._emptyFn) {
+ 		this.el.map(el => el.addEventListener(VivaCT.animationIteration, () => {
+ 			this._clearDom();
+ 			callback.call(this);
+ 		}).bind(this));
  	}
 
  	bounce(config = VivaCT.defaultConfig) {
- 		switch(query){
+ 		switch(config.q){
  			case VivaCT.enterFromInside: 
  				break;
  			case VivaCT.enterFromOutside: 
@@ -156,7 +203,27 @@
  			case VivaCT.exitToLeft: 
  				break;
  			default:
+ 				config.keyframes = [
+ 					{
+ 						at: [0,20,53,80,100],
+ 						state:['transform: translate3d(0, 0, 0)']
+ 					},
+ 					{
+ 						at: [40,43],
+ 						state:['transform: translate3d(0, -30px, 0)']
+ 					},
+ 					{
+ 						at: [70],
+ 						state:['transform: translate3d(0, -15px, 0)']
+ 					},
+ 					{
+ 						at: [90],
+ 						state:['translate3d(0, -4px, 0)']
+ 					}
+ 				]
  				break;
  		}
+ 		
+ 		VivaCT._initAnimation(config);
  	}
  }
