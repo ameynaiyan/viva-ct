6bf037a8bd7b1cda4f03066bb1a5b6f072894f3a
Amey Naiyan <ameyd9@gmail.com>
2018-08-10 12:20:15 -0400

    v1

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
 	entry: './src/viva-ct-src.js',
 	output: {
-		filename: 'viva-ct.min.js',
-		//filename: 'viva-ct.js',
+		//filename: 'viva-ct.min.js',
+		filename: 'viva-ct.js',
 		path: path.join(__dirname,'dist'),
 		library:'VCT',
 		libraryExport:['default']
@@ -13,8 +13,8 @@ module.exports = {
 		contentBase: path.join(__dirname,'dist'),
 		port: 3000
 	},
-	mode: 'production',
-	//mode: 'development',
+	//mode: 'production',
+	mode: 'development',
 	module: {
 		rules: [
 			{
